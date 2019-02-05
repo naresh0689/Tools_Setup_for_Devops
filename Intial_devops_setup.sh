031ac87d391aa2354b0dee0634d82d55148c195c
Naveen9devops <46016465+Naveen9devops@users.noreply.github.com>
2019-02-05 14:31:56 +0530

    adding kubernetes setup

--- Intial_devops_setup.sh
+++ Intial_devops_setup.sh
@@ -97,3 +97,13 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 docker --version
+# kubernetes setup
+sudo apt-get update && sudo apt-get install -y apt-transport-https && \
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list && \
+sudo apt-get update && \
+sudo apt-get install -y kubectl
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube && \
+  sudo cp minikube /usr/local/bin && rm minikube
