53b75f7341c69a6f1e14c3dca713ca9d03ca7c83
Naveen9devops <46016465+Naveen9devops@users.noreply.github.com>
2019-02-05 14:34:46 +0530

    Update Intial_devops_setup.sh

--- Intial_devops_setup.sh
+++ Intial_devops_setup.sh
@@ -102,8 +102,10 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https && \
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list && \
 sudo apt-get update && \
-sudo apt-get install -y kubectl
+sudo apt-get install -y kubectl kubeadm kubelet
+echo kubectl version
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube && \
   sudo cp minikube /usr/local/bin && rm minikube
+echo minikube vresion
