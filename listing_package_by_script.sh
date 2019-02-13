05eb638baa52024999ad773024bc9bc444d3a9bb
naveen <user@user.com>
2019-02-13 13:05:48 +0000

    Listing packages

--- listing_package_by_script.sh
+++ listing_package_by_script.sh
@@ -6,4 +6,5 @@ dpkg -l | grep '^.i' | tr -s ' ' | cut -d' ' -f2,3 | tr ' ' : | cut -d: -f1,2 > 
 
 cat -n installed_packages.csv > installed_packages_list.csv
 
+grep " installed " /var/log/dkpg.log > recent__installed_packeges.csv
 
