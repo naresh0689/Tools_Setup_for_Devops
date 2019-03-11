646cf66419aa8d3a4f11f144791f97ba3ab8db1c
naveenanthati3 <47098673+naveenanthati3@users.noreply.github.com>
2019-03-11 20:41:14 +0530

    Update listing_package_by_script.sh

--- listing_package_by_script.sh
+++ listing_package_by_script.sh
@@ -9,10 +9,10 @@ else
 dpkg-query -l | cut -c 5-91 | sed '1,3d' > Installed_Packages.csv
 cat Installed_Packages.csv > "Installed_Packages_List-`date '+%Y-%m-%d'`.csv"
 fi
-comp $Previous $Latest
-if  [ -e /var/log/dpkg.log ];
-then
-echo " Recently no packages are upgraded"
-else
-grep " installed " /var/log/dpkg.log > recent__installed_packeges.csv
-fi
+diff $Previous $Latest
+#if  [ -e /var/log/dpkg.log ];
+#then
+#grep " installed " /var/log/dpkg.log | sed 'y,:, ,' | awk '{ print $7,$9 }' > recent__installed_packeges.csv
+#else
+#echo " Recently no packages are upgraded"
+#fi
