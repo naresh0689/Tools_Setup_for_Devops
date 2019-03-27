7badfb1d6b4dc642c661ec3b9555655e97f60af1
naveen <naveen@naveen.com>
2019-03-27 22:41:58 +0530

    brek

---
+++ shell_scripting/break/break_exe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+match=$1  # fileName
+found=0   # set to 1 if file found in the for loop
+
+# show usage
+[ $# -eq 0 ] && { echo "Usage: $0 fileName"; exit 1; }
+# Try to find file in /etc
+for f in /etc/*
+do
+	if [ $f == "$match" ]
+	then
+	 	echo "$match file found!"
+	 	found=1 # file found
+		break   # break the for looop
+	fi
+done
+# noop file not found
+[ $found -ne 1 ] && echo "$match file not found in /etc directory"
